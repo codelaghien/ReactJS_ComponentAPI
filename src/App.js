6dd8f3f4c9a36282ac4a82c92d068413b8b07c9a
codelaghien <codelaghien.github.io>
2021-03-12 15:04:33 -0800

    createRef()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-			data: [],
-			data2: '',
+			data: '',
 		};
 
 		this.myTextInput = React.createRef();
@@ -15,55 +14,28 @@ class App extends React.Component {
 		// this.setStateHandler = this.setStateHandler.bind(this);
 	}
 
-	setStateHandler = () => {
-		const item = 'setState...';
-		// const myArray = this.state.data.slice();
-		// myArray.push(item);
-		// const myArray = [this.state.data];
-		const myArray = [...this.state.data];
-		myArray.push(item);
-		this.setState({ data: myArray });
-	};
-
-	forceUpdateHandler = () => {
-		this.forceUpdate();
-	};
-
-	findDomNodeHandler = () => {
-		const myDiv = document.getElementById('myDiv');
-		ReactDOM.findDOMNode(myDiv).style.color = 'green';
-	};
-
 	updateState = (e) => {
-		this.setState({ data2: e.target.value });
+		this.setState({ data: e.target.value });
 	};
 
 	clearInput = () => {
-		this.setState({ data2: '' });
+		this.setState({ data: '' });
 		// ReactDOM.findDOMNode(this.refs.myInput).focus();
-
 		this.myTextInput.current.focus();
 	};
 
 	componentDidMount() {
+		// this.myTextInput.current.value = 'nhập vào';
 		this.myTextInput.current.focus();
 	}
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.setStateHandler}>SET STATE</button>
-				<h4>State Array: {this.state.data}</h4>
-				<button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
-				<h4>Random number: {Math.random()}</h4>
-				<button onClick={this.findDomNodeHandler}>
-					FIND DOME NODE
-				</button>
-				<div id='myDiv'>NODE</div>
-				<br />
-				<br />
+				{' '}
+				&nbsp;&nbsp;
 				<input
-					value={this.state.data2}
+					value={this.state.data}
 					onChange={this.updateState}
 					ref={this.myTextInput}
 				></input>
