6eda863740198c1af95f9d91ca4f8f64cd0f4f8f
codelaghien <codelaghien.github.io>
2021-03-11 20:47:20 -0800

    Component API

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			data: [],
+			data2: '',
+		};
+
+		// this.setStateHandler = this.setStateHandler.bind(this);
+	}
+
+	setStateHandler = () => {
+		const item = 'setState...';
+		// const myArray = this.state.data.slice();
+		// myArray.push(item);
+		// const myArray = [this.state.data];
+		const myArray = [...this.state.data];
+		myArray.push(item);
+		this.setState({ data: myArray });
+	};
+
+	forceUpdateHandler = () => {
+		this.forceUpdate();
+	};
+
+	findDomNodeHandler = () => {
+		const myDiv = document.getElementById('myDiv');
+		ReactDOM.findDOMNode(myDiv).style.color = 'green';
+	};
+
+	updateState = (e) => {
+		this.setState({ data2: e.target.value });
+	};
+	clearInput = () => {
+		this.setState({ data2: '' });
+		ReactDOM.findDOMNode(this.refs.myInput).focus();
+	};
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.setStateHandler}>SET STATE</button>
+				<h4>State Array: {this.state.data}</h4>
+				<button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
+				<h4>Random number: {Math.random()}</h4>
+				<button onClick={this.findDomNodeHandler}>
+					FIND DOME NODE
+				</button>
+				<div id='myDiv'>NODE</div>
+				<br />
+				<br />
+				<input
+					value={this.state.data2}
+					onChange={this.updateState}
+					ref='myInput'
+				></input>
+				<button onClick={this.clearInput}>CLEAR</button>
+				<h4>{this.state.data}</h4>
+			</div>
+		);
+	}
+}
+export default App;
